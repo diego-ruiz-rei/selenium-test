727edde5694cdfbd662f8a14f13aa87a20122604
Kevin Song <kevin.song@reisystems.com>
2017-01-20 17:51:03 -0500

    Refactor - move functions to common utils

--- src/test/java/gov/gsa/Tests/OpportunitiesObjectViewTest.java
+++ src/test/java/gov/gsa/Tests/OpportunitiesObjectViewTest.java
@@ -11,14 +11,13 @@ import org.junit.Test;
 import org.junit.runners.MethodSorters;
 
 import java.util.ArrayList;
-import java.util.List;
-import java.util.function.Consumer;
 
+import static gov.gsa.Utilities.CommonUtils.testFieldExists;
 import static junit.framework.Assert.assertEquals;
 import static junit.framework.Assert.assertTrue;
 
 @FixMethodOrder(MethodSorters.NAME_ASCENDING)
-public class OpportunitiesObjectViewTest extends Base{
+public class OpportunitiesObjectViewTest extends Base {
 
     @BeforeClass
     public static void start() throws InterruptedException {
@@ -33,55 +32,15 @@ public class OpportunitiesObjectViewTest extends Base{
         }
     }
 
-    /**
-     * Process a {@code List} containing information about a field.
-     * The field's label and data are split up and processing delegates to the appropriate {@code Consumer}.
-     *
-     * Precondition: {@code List} contains field's label in first element and field's data in second element
-     * Postcondition: {@code labelConsumer} is passed field's label and {@code dataConsumer} is passed field's data
-     */
-    public static void processField(List<String> fieldParse, Consumer<String> labelConsumer, Consumer<String> dataConsumer) {
-        labelConsumer.accept(fieldParse.get(0));
-        dataConsumer.accept(fieldParse.get(1));
-    }
-
-    /** Convenience method to test that a field exists; see overloaded method for details **/
-    public static void testFieldExists(String fieldLabel, ArrayList<String> fieldParse) {
-        testFieldExists(fieldLabel, fieldParse, true);
-    }
-
-    /**
-     * Verifies either that a field that should exist does and has some data,
-     * or that a field that shouldn't exist does not and has no data, based on the value of {@code shouldExist} param
-     *
-     * @param fieldLabel The label that the field is expected to have (this should also be the field's name)
-     * @param fieldParse {@code List} containing information about a field
-     * @param shouldExist {@code true} if the field is expected to exist, else {@code false}
-     *
-     * Precondition: {@code List} contains field's label in first element and field's data in second element
-     */
-    public static void testFieldExists(String fieldLabel, List<String> fieldParse, boolean shouldExist) {
-        String labelErrorMessage = fieldLabel + (shouldExist ? " label does not exist" : " label exists");
-        String dataErrorMessage = fieldLabel + (shouldExist ? " data is empty" : " data is not empty");
-
-        processField(
-            fieldParse,
-            label -> assertEquals(labelErrorMessage, label.contains(fieldLabel), shouldExist), // check label
-            data -> assertEquals(dataErrorMessage, !data.isEmpty(), shouldExist) // check data
-        );
-
-        System.out.println(fieldLabel + (shouldExist ? " Label and Data exist" : " Label and Data do not exist"));
-    }
-
     @Test
-    public void oppTitleTest(){
+    public void oppTitleTest() {
         assertTrue(OpportunitiesObjectViewPage.oppTitle());
         System.out.println("Title is Present in the Object View page");
     }
 
     @Test
     public void oppSideMenuItemsTest() {
-        assertEquals("Overview is not found in Side menu","General Information", OpportunitiesObjectViewPage.generalInformation());
+        assertEquals("Overview is not found in Side menu", "General Information", OpportunitiesObjectViewPage.generalInformation());
         assertEquals("Classification", OpportunitiesObjectViewPage.classification());
         assertEquals("Synopsis/Description", OpportunitiesObjectViewPage.synopsis());
         assertEquals("Packages", OpportunitiesObjectViewPage.packages());
@@ -154,9 +113,8 @@ public class OpportunitiesObjectViewTest extends Base{
     @Test
     public void contractingOfficeTest() {
         ArrayList<String> contracting = OpportunitiesObjectViewPage.contractingOffice();
-        for (String temp : contracting)
-        {
-            assertTrue(temp.length() >1);
+        for (String temp : contracting) {
+            assertTrue(temp.length() > 1);
         }
 
         System.out.println("Contracting Office Data exists");
@@ -172,7 +130,7 @@ public class OpportunitiesObjectViewTest extends Base{
     }
 
     @AfterClass
-    public static void end(){
+    public static void end() {
         closeOut();
     }
 }
