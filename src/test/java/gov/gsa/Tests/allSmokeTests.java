0844bf42cf2d3671f3b2d79b2120c77eba91e308
Michael Kellogg <michael.kellogg@C02RV0RHG8WL.local>
2017-01-31 16:50:09 -0500

    changes all smoke tests code back

--- src/test/java/gov/gsa/Tests/allSmokeTests.java
+++ src/test/java/gov/gsa/Tests/allSmokeTests.java
@@ -13,8 +13,8 @@ import org.junit.runners.Suite;
 // this class uses annotations to run all the test classes in one location
 //
 @RunWith(Suite.class)
-//@Suite.SuiteClasses({HomepageTest.class, FACObjectViewTest.class, OpportunitiesObjectViewTest.class, SearchTest.class,
-//OpportunitiesPackagesTest.class, FederalHierarchyObjectViewTest.class})
-@Suite.SuiteClasses({HomepageTest.class, FederalHierarchyObjectViewTest.class})
+@Suite.SuiteClasses({HomepageTest.class, FACObjectViewTest.class, OpportunitiesObjectViewTest.class, SearchTest.class,
+OpportunitiesPackagesTest.class, FederalHierarchyObjectViewTest.class})
+//@Suite.SuiteClasses({HomepageTest.class, FederalHierarchyObjectViewTest.class})
 public class allSmokeTests extends Base{
 }
